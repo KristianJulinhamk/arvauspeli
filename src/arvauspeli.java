fce9a01e5dff6b1a6d9767d9433008ed548aa712
KristianJulinhamk <kristian.julin@student.hamk.fi>
2024-10-25 22:35:32 +0300

    Initial commit

---
+++ src/arvauspeli.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class arvauspeli {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String correctAnswer = "Kristian";
+
+        // arvauskertojen muuttuja
+        int arvausKerrat = 0;
+        String arvaus = "";
+
+        // loop kysyy arvausta
+        while (true) {
+            // kysyy nimen
+            System.out.println("Arvaa nimeni (kirjoita loppu lopettaaksesi)");
+            arvaus = scanner.nextLine();
+
+            // katsoo haluaako kayttaja lopettaa
+            if (arvaus.equalsIgnoreCase("loppu")) {
+                System.out.println("Päätit lopettaa.");
+                break;
+            }
+
+            // lisää arvauskerran vain jos arvaus ei ole stop
+            arvausKerrat++;
+
+            // katsoo onko arvaus oikein
+            if (arvaus.equalsIgnoreCase(correctAnswer)) {
+                System.out.println("Onneksi olkoon!");
+                break;  // poistuu loopista jos arvaus on oikein
+            } else {
+                System.out.println("Arvaa uudestaan.");
+            }
+        }
+
+        // printtaa arvauskerrat
+        System.out.println("Arvasit " + arvausKerrat + " kertaa.");
+
+        scanner.close();
+    }
+}
+
